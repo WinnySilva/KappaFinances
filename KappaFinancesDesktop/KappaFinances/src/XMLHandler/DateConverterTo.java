147fd5378008df2a0fef5ced633306e5e78587a3
SimaoPereba <SimaoPereba>
2016-04-22 13:27:08 -0300

    Embelezamento da classe DateConverterTo
    Signed-off-by: SimaoPereba <SimaoPereba>

--- KappaFinancesDesktop/KappaFinances/src/XMLHandler/DateConverterTo.java
+++ KappaFinancesDesktop/KappaFinances/src/XMLHandler/DateConverterTo.java
@@ -18,8 +18,11 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 /**
- *
- * @author Usuário
+ * Essa classe serve para alterar o formato da data que será passada ao XML
+ * Antes ela era armazenada em milissegundos a partir de 1/1/1970. Agora ela
+ * está mais supimpa, bonita, elegante e charmosa.
+ * 
+ * @author Simão
  */
 public class DateConverterTo implements Converter {
 
@@ -27,22 +30,33 @@ public class DateConverterTo implements Converter {
             "MMMM dd, yyyy HH:mm:ss");
 
     @Override
-    public boolean canConvert(Class clazz) {
-        // This converter is only for Calendar fields.
+    public boolean canConvert(Class clazz) 
+    {
+        // Converte só a classe calendário
         return Calendar.class.isAssignableFrom(clazz);
     }
 
+    /**
+     * Método para deserealizar o tipo Calendar
+     * 
+     */
     @Override
     public void marshal(Object value, HierarchicalStreamWriter writer,
-            MarshallingContext context) {
+            MarshallingContext context) 
+    {
         Calendar calendar = (Calendar) value;
         Date date = calendar.getTime();
         writer.setValue(formatter.format(date));
     }
-
+    
+    /**
+     * Método para deserealizar o tipo Calendar
+     * 
+     */
     @Override
     public Object unmarshal(HierarchicalStreamReader reader,
-            UnmarshallingContext context) {
+            UnmarshallingContext context) 
+    {
         GregorianCalendar calendar = new GregorianCalendar();
         try {
             calendar.setTime(formatter.parse(reader.getValue()));
