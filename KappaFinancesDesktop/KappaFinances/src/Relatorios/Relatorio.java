af13dd1f8790be912d5908968a06fc0d8c238d08
Winny Silva <winny.silva@yahoo.com.br>
2016-04-30 01:29:51 -0300

    Graficos operacionais agora, porém a classe relatório está sendo atrasada devido ao ñ termino do pacote finanças

--- KappaFinancesDesktop/KappaFinances/src/Relatorios/Relatorio.java
+++ KappaFinancesDesktop/KappaFinances/src/Relatorios/Relatorio.java
@@ -4,6 +4,7 @@ import Financas.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Observer;
+import java.util.Random;
 import javafx.application.Application;
 import javafx.scene.chart.Chart;
 /*
@@ -16,12 +17,18 @@ import javafx.scene.chart.Chart;
  * @author Winny S
  */
 public abstract class Relatorio extends Application implements Observer  {
+    protected String titulo ="Despesas";
     protected Contabilidade contas;
     protected ArrayList<Dado> dados;
     protected class Dado{
         public String categoria_s;
         public int categeria_i;
-        public int total;
+        public double total;
+        Dado(String s, int i, double t){
+            this.categeria_i=i;
+            this.categoria_s=s;
+            this.total=t;
+        }
     }
         
     protected enum despesas {
@@ -29,6 +36,14 @@ public abstract class Relatorio extends Application implements Observer  {
     }
     Relatorio(Contabilidade con){
         this.contas = con;
+    }
+    Relatorio(){
+        Random r = new Random();
+      dados = new ArrayList();
+      for(int i=0;i<10;i++){
+        
+          dados.add(new Dado("Tenso"+r.nextInt(200),8,r.nextInt(700) ));
+      }
     }
     public abstract Chart  geraGrafico();
     /**
@@ -39,7 +54,6 @@ public abstract class Relatorio extends Application implements Observer  {
        this.dados = new ArrayList();
        Iterator<Financa> it = financas.iterator();
        Financa aux;
-       
        while(it.hasNext()){
            aux = it.next();
            if( (aux instanceof Receita ) ){
